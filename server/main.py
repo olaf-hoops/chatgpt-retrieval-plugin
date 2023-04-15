2f3798117920e58aabb93f1ec3481b9f2759deae
olaf-hoops <35558544+olaf-hoops@users.noreply.github.com>
2023-04-15 15:11:40 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -94,7 +94,7 @@ async def upsert(
 async def agent(request: AgentRequest):
     try:
         results = await get_agent_response(request.query_text)
-        return {"response": result["output"]}
+        return {"response": results["output"]}
     except Exception as e:
         print("Error:", e)
         raise HTTPException(status_code=500, detail="Internal Service Error")
