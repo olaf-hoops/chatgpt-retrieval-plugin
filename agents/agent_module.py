f52e9f813106179f687e8ad7ce615646e2fc1bd3
olaf-hoops <35558544+olaf-hoops@users.noreply.github.com>
2023-04-17 20:30:26 +0200

    Update agent_module.py

--- agents/agent_module.py
+++ agents/agent_module.py
@@ -30,7 +30,7 @@ retriever = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=v
 search = GoogleSearchAPIWrapper()
 
 # Initialize Tools
-db_desc = "Use this tool to answer user questions about bank related topics lika BaFin."
+db_desc = "Use this tool to answer user questions about Volksbank internals (e.g. internal guidelines, sales data or work instructions) and related topics lika BaFin publications."
 search_desc = "Use this tool to answer questions about current events or the current state of the world. the input to this should be a single search term."
 tools = [Tool(func=retriever.run, description=db_desc, name='Bank DB'), 
          Tool(func=search.run, description=search_desc, name='Search Internet')]
@@ -46,7 +46,7 @@ conversational_agent = initialize_agent(
     memory=memory,
 )
 
-sys_msg = "You are a helpful chatbot that answers the user's questions. It's important that everytime you generate a 'response' it is in german language."
+sys_msg = "You are a helpful Volksbank assistant that answers the user's questions. It's very important that everytime you generate a 'response' it is in german language."
 prompt = conversational_agent.agent.create_prompt(system_message=sys_msg, tools=tools)
 conversational_agent.agent.llm_chain.prompt = prompt
 
